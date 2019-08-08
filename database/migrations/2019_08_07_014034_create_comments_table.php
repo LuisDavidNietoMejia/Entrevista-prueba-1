4d7b05a146ff344075aeea00c77c87f72e9b69ff
Luis David Nieto Mejia <luisdavidnietomejia@gmail.com>
2019-08-08 09:38:56 -0400

    change migration

--- database/migrations/2019_08_07_014034_create_comments_table.php
+++ database/migrations/2019_08_07_014034_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('content');
+            $table->enum('status',['Aprobado','Desaprobado'])->default('Desaprobado');
             $table->bigInteger('publication_id')->unsigned();
             $table->foreign('publication_id')->references('id')->on('publications')->onDelete('cascade');
             $table->bigInteger('user_id')->unsigned();
